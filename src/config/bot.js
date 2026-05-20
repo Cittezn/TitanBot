0d242532337b8fd413b4bb208f0efab790f44aef
Cittezn <cittezn76@gmail.com>
2026-05-20 07:27:11 -0300

    .

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made with crack cocaine",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
@@ -136,7 +136,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "Jimithy",
       // Footer icon URL (null = no icon).
       icon: null,
     },
